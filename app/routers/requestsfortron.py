cc00671503fc0dc749d053acaa76cfb8cf5c57be
Ilya Kizilov <i.g.kizilov@yandex.ru>
2025-03-27 18:52:20 +0200

    version N.4

--- app/routers/requestsfortron.py
+++ app/routers/requestsfortron.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.service.gettronaccountinfo import get_tron_account_info
 from app.schemas import TronAddressRequest, TronAddressResponse
 from app.database import get_session
+from app.models import RequestsToTron
 
 router = APIRouter(prefix="/requestsfortron", tags=["requestsfortron"])
 
@@ -14,4 +15,5 @@ async def create_request(
         db: AsyncSession = Depends(get_session)
 ):
     data = await get_tron_account_info(addresdata.address)
+    upload = RequestsToTron(tron_address=data.address)
 
