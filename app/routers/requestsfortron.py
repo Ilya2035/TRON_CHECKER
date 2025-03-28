8dc439eb85a64bd6c85b48a65cedd7d46d065e29
Ilya Kizilov <i.g.kizilov@yandex.ru>
2025-03-28 14:16:58 +0200

    version N.1

--- app/routers/requestsfortron.py
+++ app/routers/requestsfortron.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.service.gettronaccountinfo import get_tron_account_info
-from app.schemas import TronAddressRequest, TronAddressResponse
+from app.schemas import TronAddressRequest, TronAddressResponse, Listofrequests
 from app.database import get_session
 from app.models import RequestsToTron
+from app.config import PAGES_SIZE
 
 router = APIRouter(prefix="/requestsfortron", tags=["requestsfortron"])
 
@@ -20,3 +21,12 @@ async def create_request(
     await db.commit()
     await db.refresh(upload)
     return data
+
+
+@router.get("/", response_model=Listofrequests)
+async def get_requests_list(
+        page: int = Query(1, ge=1, description="Номер страницы (начиная с 1)"),
+
+        db: AsyncSession = Depends(get_session)
+):
+    pass
--- app/schemas.py
+++ app/schemas.py
@@ -9,3 +9,9 @@ class TronAddressResponse(BaseModel):
     bandwidth: int
     energy: int
     balance_trx: float
+
+
+class Listofrequests(BaseModel):
+    id: int
+    tron_address: str
+    request_time: str
